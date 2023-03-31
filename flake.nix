e0877229ca6054b64e8ec2a7515325d9841a7998
Chris Jones <chris.jones@ironcorelabs.com>
2023-03-31 11:39:52 -0600

    Add a simple flake. (#1)
    * Add a simple flake.
    
    * Update flake.nix
    
    The action is node16, flake should be too
    
    * Update flake.nix
    
    This action uses `npm` instead of `yarn` like our other projects.
    
    * Add flakey gitignore
    
    ---------
    
    Co-authored-by: Murph Murphy <murph@clurictec.com>

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "rust-install";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        lib = import <nixpkgs/lib>;
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      rec {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs.nodePackages; 
+          [
+            pkgs.nodejs-16_x
+          ];
+        };
+      });
+}
